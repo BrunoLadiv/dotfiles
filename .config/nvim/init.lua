81a3d1eff0e05bafbc6f9e3f6fa31417602c1af2
Bruno <brunokasprik@gmail.com>
2024-02-13 23:03:55 -0300

    handle navigation on vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 if vim.g.vscode then
   vim.api.nvim_exec(
     [[
@@ -27,7 +28,6 @@ if vim.g.vscode then
     false
   )
 end
-require("config.lazy")
 
 -- win32yank (make copy and paste work from vim wsl2 to windows)
 vim.api.nvim_set_option("clipboard", "unnamedplus")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,34 +2,34 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set('x', 'ys', '<Plug>VSurround', { silent = true })
+vim.keymap.set("x", "ys", "<Plug>VSurround", { silent = true })
 
 --vscode keys
 if vim.g.vscode then
-  vim.api.nvim_del_keymap('n', '<C-/>')
+  vim.api.nvim_del_keymap("n", "<C-/>")
   --visual and normal mode comments
-  vim.keymap.set('n', '<C-/>', '<Plug>VSCodeCommentaryLinegv', { silent = true })
-  vim.keymap.set('x', '<C-/>', '<Plug>VSCodeCommentarygv', { silent = true })
+  vim.keymap.set("n", "<C-/>", "<Plug>VSCodeCommentaryLinegv", { silent = true })
+  vim.keymap.set("x", "<C-/>", "<Plug>VSCodeCommentarygv", { silent = true })
 
-  vim.keymap.set('n', '<C-j>', ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
-  vim.keymap.set('x', '<C-j>', ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+  vim.keymap.set("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+  vim.keymap.set("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
 
   -- need to find a way for better line movement in vscode
   -- vim.keymap.set('x', '<M-j>', ':call VSCodeNotify("editor.action.moveLinesDownAction")<CR>', { silent = true })
   -- vim.keymap.set('x', '<M-k>', ':call VSCodeNotify("editor.action.moveLinesUpAction")<CR>', { silent = true })
   -- vim.keymap.set('n', '<M-j>', ':call VSCodeNotify("editor.action.moveLinesDownAction")<CR>', { silent = true })
-  -- vim.keymap.set('n', '<M-k>', ':call VSCodeNotify("editor.action.moveLinesUpAction")<CR>', { silent = true })
-  vim.keymap.set('n', '<C-k>', ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
-  vim.keymap.set('x', '<C-k>', ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
-
-  vim.keymap.set('n', '<C-h>', ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
-  vim.keymap.set('x', '<C-h>', ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
-
-  vim.keymap.set('n', '<C-l>', ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
-  vim.keymap.set('x', '<C-l>', ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
-
-  vim.keymap.set('n', '<C-w>_', ':<C-u>call VSCodeNotify("workbench.action.toggleEditorWidths")<CR>', { silent = true })
-
-  vim.keymap.set('n', '<Space>', ':call VSCodeNotify("whichkey.show")<CR>', { silent = true })
-  vim.keymap.set('x', '<Space>', ':call VSCodeNotify("whichkey.show")<CR>', { silent = true })
+  -- vim.keymap.set('n', '<M-k>', ':call VSCodeNoti
+  -- vim.keymap.set('n', '<C-k>', ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+  -- vim.keymap.set('x', '<C-k>', ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+  --
+  -- vim.keymap.set('n', '<C-h>', ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+  -- vim.keymap.set('x', '<C-h>', ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+  --
+  -- vim.keymap.set('n', '<C-l>', ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+  -- vim.keymap.set('x', '<C-l>', ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+  --
+  -- vim.keymap.set('n', '<C-w>_', ':<C-u>call VSCodeNotify("workbench.action.toggleEditorWidths")<CR>', { silent = true })
+  --
+  -- vim.keymap.set('n', '<Space>', ':call VSCodeNotify("whichkey.show")<CR>', { silent = true })
+  -- vim.keymap.set('x', '<Space>', ':call VSCodeNotify("whichkey.show")<CR>', { silent = true })
 end
